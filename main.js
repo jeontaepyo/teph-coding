f490b0bd52c7be83b2bfe369b8907ce7400c22f7
jtp9106 <jtp9106@gmail.com>
2026-02-18 23:43:45 +0000

    Update project

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-// Add JS here
+const numbersContainer = document.getElementById('numbers');
+const generateBtn = document.getElementById('generate-btn');
+
+generateBtn.addEventListener('click', () => {
+  numbersContainer.innerHTML = '';
+  const lottoNumbers = new Set();
+  while (lottoNumbers.size < 6) {
+    lottoNumbers.add(Math.floor(Math.random() * 45) + 1);
+  }
+
+  const sortedNumbers = Array.from(lottoNumbers).sort((a, b) => a - b);
+
+  sortedNumbers.forEach(number => {
+    const numberDiv = document.createElement('div');
+    numberDiv.classList.add('number');
+    numberDiv.textContent = number;
+    numbersContainer.appendChild(numberDiv);
+  });
+});
